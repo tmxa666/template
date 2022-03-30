6879543526e9933709143a05363f9b8a3eee8590
wangheng87 <82043525@163.com>
2022-03-30 16:38:01 +0800

    first commit

---
+++ web/start.js
@@ -0,0 +1,4 @@
+ var init=require("../../../yujiang.Foil.Node/src/yjApp.init.js");
+ var path=require("path");
+ init.setConfigFile(path.join(__dirname,"./config.js"));
+ require("../../../yujiang.Foil.Node/src/yjApp.js");
